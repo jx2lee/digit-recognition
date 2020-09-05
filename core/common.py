b48eabc323ebdbc8d33ab3af8636833c14d9ee2b
jx2lee <jaejun.lee.1991@gmail.com>
2020-09-06 00:59:52 +0900

    model

---
+++ core/common.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import logging
+
+
+def print_info(string):
+    print('\033[31m \033[43m' + '[INFO]'+ '\033[0m' + ' ' + string)
+
+
+def print_error(string):
+    print('\033[37m \033[101m' + '[ERROR]'+ '\033[0m' + ' ' + string)
+
+
+def make_logger(name=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+    
+    logger.addHandler(console)
+    return logger
