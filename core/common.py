03d44f82b66b9e581fa69f5bb898010a651beafe
jx2lee <jaejun.lee.1991@gmail.com>
2020-09-06 13:39:37 +0900

    move image function

--- core/common.py
+++ core/common.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import cv2
 import logging
+import numpy as np
 
 
 def print_info(string):
@@ -22,3 +24,30 @@ def make_logger(name=None):
     
     logger.addHandler(console)
     return logger
+
+
+def resize_input(image):
+    """
+    Resize image
+    :param image: image path for resize
+    :return: res: resized image
+    """
+    img = cv2.imread(image)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (128, 128))
+    img = np.expand_dims(img, 2)
+    res = np.expand_dims(img, 0)
+    return res
+
+
+def image_to_float(img_path):
+    """
+    Image to float function
+    :param img_path: Path To float image file
+    :return thresh: 2d array after gray-scale image
+    """
+    raw = cv2.imread(img_path)
+    gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+    gray = np.expand_dims(gray, axis=2)
+
+    return gray
